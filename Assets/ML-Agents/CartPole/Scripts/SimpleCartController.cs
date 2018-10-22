31b0ae982819bcd99a152ce03ff40235e87bf313
bbueno5000 <bbueno5000@gmail.com>
2018-10-22 12:20:06 -0600

    added wheeled cart-pole

---
+++ Assets/ML-Agents/CartPole/Scripts/SimpleCartController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class SimpleCartController : MonoBehaviour
+{
+    public List<AxleInfo> axleInfos; // the information about each individual axle
+    public float maxMotorTorque; // maximum torque the motor can apply to wheel
+
+    public void FixedUpdate ()
+    {
+        float motor = maxMotorTorque * Input.GetAxis("Vertical");
+
+        foreach (AxleInfo axleInfo in axleInfos)
+        {
+            if (axleInfo.motor)
+            {
+                axleInfo.leftWheel.motorTorque = motor;
+                axleInfo.rightWheel.motorTorque = motor;
+            }
+        }
+    }
+}
